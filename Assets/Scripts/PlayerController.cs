3d49516ec031b1338be8aa85fd014a1e0ee1cff0
mattprateep <matthewprateep@gmail.com>
2022-09-29 18:47:00 -0400

    PlayerController
    Added input and player controller script to player so ball can move around

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour {
-    
-    public float speed;
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
 
     private Rigidbody rb;
+    private float movementX;
+    private float movementY;
 
-    void Start ()
+    // Start is called before the first frame update
+
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate ()
+    void OnMove(InputValue movementValue)
     {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
+        Vector2 movementVector = movementValue.Get<Vector2>();
 
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
 
-        rb.AddForce (movement * speed);
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        
+        rb.AddForce(movement * speed);
     }
 }
